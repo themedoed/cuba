13fea93d1cdb855d94cd846d7bc59e8cb3b229f3
themedoed <93190084+themedoed@users.noreply.github.com>
2021-11-06 03:51:25 +0700

    Сделал адаптив, заменил тайтл

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ $(function(){
     prevArrow: '<button type="button" class="slick-prev"><img src="images/Arrow-left.svg" alt="Left-Arrow"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/Arrow-right.svg" alt="Right-Arrow"></button>'
   }); 
+
+  $('.menu__btn').on('click', function(){
+      $('.menu__items').slideToggle();
+  });
 });
