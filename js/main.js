3523963c8587b642fb88f7fe49007a74932e1cc1
themedoed <93190084+themedoed@users.noreply.github.com>
2021-11-05 19:06:13 +0700

    Вэб без адаптива

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+  $('.slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/Arrow-left.svg" alt="Left-Arrow"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/Arrow-right.svg" alt="Right-Arrow"></button>'
+  }); 
+});
